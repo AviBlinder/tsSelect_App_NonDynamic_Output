f6e996787fb43391b1a888ab8ee4d478460fb708
Avi Blinder <aviblinder@gmail.com>
2016-12-03 18:23:24 +0200

    changing UI look&feel --> FluidRows

--- ui.R
+++ ui.R
@@ -8,54 +8,40 @@ shinyUI(fluidPage(
     
        titlePanel(""),
     
+      dygraphOutput("dygraph_plot") ,
+
+      hr(),
+
+    fluidRow(
+      column(4,
+             h4("Starting Date"),
+             dateInput("date",  value = "2014-01-01",label="")
+      ),
+      column(4, offset = 0,
+             h4("Forecasted Periods"),
+             sliderInput("Slider",label="", min = 1, 
+                         max = 20, value = 10)
+      ),
+    column(4,
+           h4("T.S. Frequency"),
+           radioButtons("Frequency", label = "",
+                        choices = list(
+#                         "Year" = 1, 
+                          "Quarter" = 4, 
+                          "Month" = 12),
+#                         "Weekly" = 7),
+                        selected = 12)
+           
+           ),
+    column(12,
+           h4("Time Series Input"),
+           p("Enter an array of numbers, separated by commas.") ,
+           p("Zero values are not allowed."),
+           textInput("ts_input",label=""), 
+           value = "0")
     
-    # Sidebar with a slider input for the number of bins
-    sidebarLayout(
-        sidebarPanel(
-            
-#            helpText("Forecast Time Series based on comparing among around 20 differet models."),
-            helpText("The first time the application is executed, a 
-                     demo is performed."),
-            helpText ("The process may take a few seconds..."),
-            
-#            helpText("Select the Starting Date of the Time Series"),
-            dateInput("date", label = h3("Starting Date"), value = "2014-01-01"),
- 
-            br(),       
-#            helpText("Select how many periods to forecast"),
-            sliderInput("Slider", label = h3("Forecasted Periods"), min = 1, 
-                        max = 20, value = 10),
-            
-            br(),
-            p("Enter an array of numbers, separated by commas.") ,
-            p("Zero values are not allowed."),
- 
-            textInput("ts_input", label = h3("Time Series input"), 
-                      value = "0"),
-            
-#            helpText("please select whether each number is a Yearly, Quarterly or Monthly observation"),
-             helpText("please select whether each number is a  Quarterly or Monthly observation : "),
-
-            radioButtons("Frequency", label = h3("Time Series's Frequency"),
-                         choices = list(
-#                             "Year" = 1, 
-                             "Quarter" = 4, 
-                              "Month" = 12),
-#                             "Weekly" = 7),
-                         selected = 12)
-
-#                        actionButton("action", label = "SUBMIT")
-            
-        ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-
-          dygraphOutput("dygraph_plot"),
-          
-          verbatimTextOutput("log_title"),
-            
-          verbatimTextOutput("ts_input_out")
         )
-    )
+
+    
+
 ))
