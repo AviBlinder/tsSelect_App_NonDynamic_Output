9c9ce99f83cd14c9df4b6275b6be45f2971a8eb8
Avi Blinder <aviblinder@gmail.com>
2016-12-03 19:27:39 +0200

    rearanging fluidRows

--- ui.R
+++ ui.R
@@ -16,33 +16,26 @@ shinyUI(fluidPage(
     fluidRow(
       column(4,
              h4("Starting Date"),
-             dateInput("date",  value = "2014-01-01",label="")
-      ),
-      column(4, offset = 0,
+             dateInput("date",  value = "2014-01-01",label=""),
+             
              h4("Forecasted Periods"),
              sliderInput("Slider",label="", min = 1, 
                          max = 20, value = 10)
       ),
-    column(4,
-           h4("T.S. Frequency"),
-           radioButtons("Frequency", label = "",
-                        choices = list(
-#                         "Year" = 1, 
-                          "Quarter" = 4, 
-                          "Month" = 12),
-#                         "Weekly" = 7),
-                        selected = 12)
-           
-           ),
-    column(12,
-           h4("Time Series Input"),
-           p("Enter an array of numbers, separated by commas.") ,
-           p("Zero values are not allowed."),
-           textInput("ts_input",label=""), 
-           value = "0")
-    
-        )
+      column(8,
+             h4("T.S. Frequency"),
+             radioButtons("Frequency", label = "",
+                          choices = list(
+                            #                         "Year" = 1, 
+                            "Quarter" = 4, 
+                            "Month" = 12),
+                          #                         "Weekly" = 7),
+                          selected = 12),
+               h4("Time Series Input"),
+               p("Enter an array of numbers, separated by commas. 
+                 Zero values are not allowed.") ,
 
-    
+               textInput("ts_input",label=""), 
+               value = "0"))
 
 ))
